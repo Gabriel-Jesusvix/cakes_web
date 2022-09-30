a6421a121409e4fd7a2db712444d38f3c567741a
Gabriel Jesus <gabrieldejesus16gs@gmail.com>
2022-09-30 00:34:52 -0300

    finished

--- src/components/CardMenu.tsx
+++ src/components/CardMenu.tsx
@@ -1,18 +1,40 @@
-export function CardMenu() {
+interface InfoCakes {
+  id: string;
+  name: string;
+  description: string;
+}
+
+export function CardMenu({ name, description }: InfoCakes) {
+  const IMAGE_PROVISION =
+    "https://images.unsplash.com/photo-1627834377411-8da5f4f09de8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1001&q=80";
+
   return (
-    <div className="bg-bg_button h-24 rounded-xl p-2">
-      <div className="">
-        <img
-          className="w-28 h-20 rounded-xl object-cover"
-          src="https://images.unsplash.com/photo-1602351447937-745cb720612f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=986&q=80"
-        />
-        <div className="flex-col">
-          <span>Bolo de chocolate</span>
-          <p className="text-[12px]">
-            Impossível não amar bolo de cenoura, né? Para dar aquela inovada na
-            receita, fizemos uma versão em camadas de deixar qualquer um com
-            água na boca. E o melhor de tudo: além de ser deliciosa, ela é super
-            fácil de fazer!
+    <div
+      className="
+    bg-bg_button 
+      h-25 
+      rounded-xl
+      p-2
+      flex
+      desktop: items-center
+      mb-5
+      
+      "
+    >
+      <div className="w-full flex tablet:flex">
+        <div className="justify-center flex items-center">
+          <img
+            src={IMAGE_PROVISION}
+            alt="cake"
+            className="object-cover rounded-xl h-20 w-[100px]"
+          />
+        </div>
+        <div className="px-4 max-w-xl tablet:w-full">
+          <div className="text-center ">
+            <strong className="text-primary"> {name}</strong>
+          </div>
+          <p className="text-[10px] text-secondary max-w-prose tablet:text-[10px] tablet:text-clip laptop:text-[10.5px] hover:text-clip ">
+            {description}.
           </p>
         </div>
       </div>
--- src/screens/Menu.tsx
+++ src/screens/Menu.tsx
@@ -1,7 +1,40 @@
+import { useState } from "react";
 import { CardMenu } from "../components/CardMenu";
 import { Header } from "../components/Header";
 
 export function Menu() {
+  const [infoCakes, setInfoCakes] = useState([
+    {
+      id: "1",
+      name: "Bolo de Cenoura",
+      description:
+        " Massa fofinha, aerada e de cor vibrante coroada de uma irresistível  cobertura de chocolate – às vezes crocante e firme; noutras, cremosae brilhante.",
+    },
+    {
+      id: "2",
+      name: "Bolo de Ninho",
+      description:
+        " Massa fofinha, aerada e de cor vibrante coroada de uma irresistível  cobertura de chocolate – às vezes crocante e firme; noutras, cremosae brilhante.",
+    },
+    {
+      id: "3",
+      name: "Bolo de Chocolate com Morango",
+      description:
+        " Massa fofinha, aerada e de cor vibrante coroada de uma irresistível  cobertura de chocolate – às vezes crocante e firme; noutras, cremosae brilhante.",
+    },
+    {
+      id: "4",
+      name: "Bolo de laranja",
+      description:
+        " Massa fofinha, aerada e de cor vibrante coroada de uma irresistível  cobertura de chocolate – às vezes crocante e firme; noutras, cremosae brilhante.",
+    },
+    {
+      id: "5",
+      name: "Bolo de maracujá",
+      description:
+        " Massa fofinha, aerada e de cor vibrante coroada de uma irresistível  cobertura de chocolate – às vezes crocante e firme; noutras, cremosae brilhante.",
+    },
+  ]);
   return (
     <div>
       <Header hidden />
@@ -18,8 +51,16 @@ export function Menu() {
 
         <h2 className="mt-16 text-2xl text-secondary font-medium">Menu</h2>
       </section>
-      <div className="px-6">
-        <CardMenu />
+      <div className="px-6 desktop:flex  desktop:items-center desktop:justify-center desktop:flex-col">
+        {infoCakes.map((infoCakes) => {
+          return (
+            <CardMenu
+              id={infoCakes.id}
+              name={infoCakes.name}
+              description={infoCakes.description}
+            />
+          );
+        })}
       </div>
     </div>
   );
